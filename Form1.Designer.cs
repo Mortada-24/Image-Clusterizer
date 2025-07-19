e983ea0b02088b641d45da813f73640065f2ab65
Mortada-24 <148127011+Mortada-24@users.noreply.github.com>
2025-07-19 20:48:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ImageClusterizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadImages = new System.Windows.Forms.Button();
+            this.btnCluster = new System.Windows.Forms.Button();
+            this.btnSaveClusters = new System.Windows.Forms.Button();
+            this.numClusters = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabClusters = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.numClusters)).BeginInit();
+            this.tabClusters.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoadImages
+            // 
+            this.btnLoadImages.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnLoadImages.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadImages.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLoadImages.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadImages.Name = "btnLoadImages";
+            this.btnLoadImages.Size = new System.Drawing.Size(162, 32);
+            this.btnLoadImages.TabIndex = 0;
+            this.btnLoadImages.Text = "UpLoad Images";
+            this.btnLoadImages.UseVisualStyleBackColor = false;
+            this.btnLoadImages.Click += new System.EventHandler(this.btnLoadImages_Click);
+            // 
+            // btnCluster
+            // 
+            this.btnCluster.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnCluster.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCluster.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCluster.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCluster.Location = new System.Drawing.Point(12, 50);
+            this.btnCluster.Name = "btnCluster";
+            this.btnCluster.Size = new System.Drawing.Size(162, 31);
+            this.btnCluster.TabIndex = 1;
+            this.btnCluster.Text = "Cluster Images";
+            this.btnCluster.UseVisualStyleBackColor = false;
+            this.btnCluster.Click += new System.EventHandler(this.btnCluster_Click);
+            // 
+            // btnSaveClusters
+            // 
+            this.btnSaveClusters.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnSaveClusters.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveClusters.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSaveClusters.Location = new System.Drawing.Point(12, 87);
+            this.btnSaveClusters.Name = "btnSaveClusters";
+            this.btnSaveClusters.Size = new System.Drawing.Size(162, 30);
+            this.btnSaveClusters.TabIndex = 2;
+            this.btnSaveClusters.Text = "Save Clusters";
+            this.btnSaveClusters.UseVisualStyleBackColor = false;
+            this.btnSaveClusters.Click += new System.EventHandler(this.btnSaveClusters_Click);
+            // 
+            // numClusters
+            // 
+            this.numClusters.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.numClusters.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numClusters.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.numClusters.Location = new System.Drawing.Point(528, 16);
+            this.numClusters.Name = "numClusters";
+            this.numClusters.Size = new System.Drawing.Size(120, 27);
+            this.numClusters.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(232, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Chose The Number Of Clusters";
+            // 
+            // tabClusters
+            // 
+            this.tabClusters.Controls.Add(this.tabPage1);
+            this.tabClusters.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabClusters.Location = new System.Drawing.Point(12, 142);
+            this.tabClusters.Name = "tabClusters";
+            this.tabClusters.SelectedIndex = 0;
+            this.tabClusters.Size = new System.Drawing.Size(936, 463);
+            this.tabClusters.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(928, 430);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(960, 617);
+            this.Controls.Add(this.tabClusters);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numClusters);
+            this.Controls.Add(this.btnSaveClusters);
+            this.Controls.Add(this.btnCluster);
+            this.Controls.Add(this.btnLoadImages);
+            this.Name = "Form1";
+            this.Text = " ImageClusterizer";
+            ((System.ComponentModel.ISupportInitialize)(this.numClusters)).EndInit();
+            this.tabClusters.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadImages;
+        private System.Windows.Forms.Button btnCluster;
+        private System.Windows.Forms.Button btnSaveClusters;
+        private System.Windows.Forms.NumericUpDown numClusters;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabClusters;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord;
+using Accord.MachineLearning;
+using Accord.Imaging;
+using System.IO;
+namespace ImageClusterizer
+{
+    public partial class Form1 : Form
+    {
+        private List<Bitmap> images = new List<Bitmap>();
+        private List<string> imagePaths = new List<string>();
+        private List<double[]> featureVectors = new List<double[]>();
+        private int[] clusteredLabels;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImages_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp",
+                Multiselect = true
+            })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    images.Clear();
+                    featureVectors.Clear();
+                    imagePaths.Clear();
+
+                    foreach (var file in ofd.FileNames)
+                    {
+                        Bitmap bmp = new Bitmap(file);
+                        images.Add(bmp);
+                        imagePaths.Add(file);
+
+                        // Simple RGB mean as features
+                        var stats = new ImageStatistics(bmp);
+                        double[] features = new double[]
+                        {
+                            stats.Red.Mean,
+                            stats.Green.Mean,
+                            stats.Blue.Mean
+                        };
+                        featureVectors.Add(features);
+                    }
+
+                    MessageBox.Show($"{images.Count} images loaded.");
+                }
+            }
+        }
+
+        private void btnCluster_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0 || featureVectors.Count == 0)
+            {
+                MessageBox.Show("Please load the images first.");
+                return;
+            }
+
+            int k = (int)numClusters.Value;
+
+            var kmeans = new KMeans(k);
+            var clusters = kmeans.Learn(featureVectors.ToArray());
+            int[] labels = clusters.Decide(featureVectors.ToArray());
+            clusteredLabels = labels;
+            DisplayClusters(labels, k);
+        }
+        private void DisplayClusters(int[] labels, int k)
+        {
+            tabClusters.TabPages.Clear();
+
+            // Create one tab per cluster
+            for (int i = 0; i < k; i++)
+            {
+                TabPage tab = new TabPage($"Cluster {i + 1}");
+                FlowLayoutPanel panel = new FlowLayoutPanel
+                {
+                    Dock = DockStyle.Fill,
+                    AutoScroll = true
+                };
+                tab.Controls.Add(panel);
+                tabClusters.TabPages.Add(tab);
+            }
+
+            // Add images to corresponding cluster tab
+            for (int i = 0; i < images.Count; i++)
+            {
+                int cluster = labels[i];
+                PictureBox pb = new PictureBox
+                {
+                    Image = new Bitmap(images[i], new Size(100, 100)),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Width = 100,
+                    Height = 100,
+                    Margin = new Padding(5)
+                };
+
+                var tab = tabClusters.TabPages[cluster];
+                var panel = (FlowLayoutPanel)tab.Controls[0];
+                panel.Controls.Add(pb);
+            }
+        }
+
+        private void btnSaveClusters_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0 || clusteredLabels == null)
+            {
+                MessageBox.Show("You must cluster images before saving.");
+                return;
+            }
+
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string basePath = folderDialog.SelectedPath;
+
+                    for (int i = 0; i < clusteredLabels.Length; i++)
+                    {
+                        int cluster = clusteredLabels[i];
+                        string clusterDir = Path.Combine(basePath, $"Cluster_{cluster + 1}");
+
+                        if (!Directory.Exists(clusterDir))
+                            Directory.CreateDirectory(clusterDir);
+
+                        string originalName = Path.GetFileName(imagePaths[i]);
+                        string savePath = Path.Combine(clusterDir, originalName);
+
+                        images[i].Save(savePath);
+                    }
+
+                    MessageBox.Show("Images saved to clusters folders.");
+                }
+            }
+        }
+    }
+}
